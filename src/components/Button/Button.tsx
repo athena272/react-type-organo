f431bcf6611d9156f060c8590b94301f2a8382c9
athena272 <guilhermera272@gmail.com>
2024-03-31 17:32:54 -0300

    Create Button.tsx

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react'
+import styles from './Button.module.scss'
+
+interface ButtonProps {
+    children: ReactNode
+}
+
+export default function Button({ children }: ButtonProps) {
+
+    return (
+        <button className={styles.button}>
+            {children}
+        </button>
+    )
+}
